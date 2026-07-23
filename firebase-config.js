0793df009020cd80854e9a76c8d4142b3033ccfb
almalamh <almalamh2070@gmail.com>
2026-07-23 14:21:30 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,12 @@
 
+var firebaseConfig = {
+    apiKey: "AIzaSyCOXdd8rSvTyILCvYqk5pS_WoaHJs2IxC0",
+    authDomain: "abou-maheeb.firebaseapp.com",
+    projectId: "abou-maheeb",
+    storageBucket: "abou-maheeb.firebasestorage.app",
+    messagingSenderId: "591919493287",
+    appId: "1:591919493287:web:4b55a5e6fadf7981f8eb82"
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
