ccdc58c672532c5bc894c21418003d49b9eb448d
almalamh <almalamh2070@gmail.com>
2026-07-26 13:13:43 +0300

    Update CACHE_NAME to version 4

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ firebase.initializeApp({
 });
 
 var messaging = firebase.messaging();
-var CACHE_NAME = 'abou-maheeb-v3';
+var CACHE_NAME = 'abou-maheeb-v4';
 var urlsToCache = [
     './',
     './menu.html',
