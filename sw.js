1d01b3ec4d8a1415bda7ce9965f1194c06efe776
almalamh <almalamh2070@gmail.com>
2026-07-27 15:09:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ firebase.initializeApp({
 });
 
 var messaging = firebase.messaging();
-var CACHE_NAME = 'abou-maheeb-v6';
+var CACHE_NAME = 'abou-maheeb-v7';
 var urlsToCache = [
     './',
     './menu.html',
